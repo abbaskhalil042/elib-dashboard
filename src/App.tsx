12e4385408876a08cbfd3590770d7afbb0ae73c8
Abbas khalil <khalilabbas389@gmail.com>
2024-07-04 16:18:47 -0700

    test shadcn ui button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
+    <>
+   
     <div>App</div>
+    <Button>click me</Button>
+    </>
   )
 }
 
